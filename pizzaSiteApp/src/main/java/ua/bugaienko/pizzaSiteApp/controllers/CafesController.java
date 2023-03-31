6eaf7a13a38d6707ed7b25ef4707d1987eb89254
Bugaienko <sergiibugaienko@gmail.com>
2023-03-31 18:10:59 +0200

    cafes done

--- pizzaSiteApp/src/main/java/ua/bugaienko/pizzaSiteApp/controllers/CafesController.java
+++ pizzaSiteApp/src/main/java/ua/bugaienko/pizzaSiteApp/controllers/CafesController.java
@@ -6,9 +6,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.bugaienko.pizzaSiteApp.models.Cafe;
 import ua.bugaienko.pizzaSiteApp.models.Pizza;
 import ua.bugaienko.pizzaSiteApp.services.PizzaService;
 import ua.bugaienko.pizzaSiteApp.util.UserUtil;
+
+import java.util.List;
 
 /**
  * @author Sergii Bugaienko
@@ -31,6 +34,8 @@ public class CafesController {
     public String pizzaAvailability(@PathVariable("pizzaId") int pizzaId, Model model) {
         model.addAttribute("user", userUtil.getActiveUser());
         Pizza pizza = pizzaService.findById(pizzaId);
+        List<Cafe> cafes = pizza.getCafes();
+        System.out.println(cafes);
         model.addAttribute("pizza", pizza);
         return "cafe/pizzaSearch";
     }
