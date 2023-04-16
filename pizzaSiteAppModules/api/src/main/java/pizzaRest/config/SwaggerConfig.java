4acb9545fdaa8017154d5165dd6e426eb03d4185
Bugaienko <sergiibugaienko@gmail.com>
2023-04-16 18:37:39 +0200

    some working

--- pizzaSiteAppModules/api/src/main/java/pizzaRest/config/SwaggerConfig.java
+++ pizzaSiteAppModules/api/src/main/java/pizzaRest/config/SwaggerConfig.java
@@ -1,12 +1,21 @@
 package pizzaRest.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 
 /**
  * @author Sergii Bugaienko
@@ -16,12 +25,52 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+//    @Bean
+//    public Docket api(){
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("pizzaRest.controllers.rest"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+
     @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2)
+    public Docket customImplementation(){
+        return new Docket(DocumentationType.OAS_30)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("pizzaRest.controllers.rest"))
-                .paths(PathSelectors.any())
+                .build()
+                .directModelSubstitute(org.threeten.bp.LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(org.threeten.bp.OffsetDateTime.class, java.util.Date.class)
+                .apiInfo(apiInfo());
+    }
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Pizza project API")
+                .description("Pizza API allows you to remotely manage the pizzeria. ### Check out its awesome features: * Authorization is required to work with the API. Authorization is done by the bearer-token 😎 ")
+                .license("")
+                .licenseUrl("http://unlicense.org")
+                .termsOfServiceUrl("")
+                .version("1.0.0")
+                .contact(new Contact("","", ""))
                 .build();
     }
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Pizza project API")
+                        .description("Pizza API allows you to remotely manage the pizzeria. ### Check out its awesome features: * Authorization is required to work with the API. Authorization is done by the bearer-token 😎 ")
+                        .termsOfService("")
+                        .version("1.0.0")
+                        .license(new License()
+                                .name("")
+                                .url("http://unlicense.org"))
+                        .contact(new io.swagger.v3.oas.models.info.Contact()
+                                .email("")));
+    }
+
+
 }
---
+++ pizzaSiteAppModules/api/src/main/java/pizzaRest/controllers/interfases/CafeControllerInt.java
@@ -0,0 +1,4 @@
+package pizzaRest.controllers.interfases;
+
+public interface CafeControllerInt {
+}
